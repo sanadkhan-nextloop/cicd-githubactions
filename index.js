680c6c246046d167dfd864c7292847cee27b8a62
snanadkhan-nextloop <skhan@nextlooptechnologies.com>
2023-09-11 11:28:56 +0530

    file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ message: "From backend"})
+});
+
+app.listen(3000, () => {
+  console.log("App listening on 3000")
+});
